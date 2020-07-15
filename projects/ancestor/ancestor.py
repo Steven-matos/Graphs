33ef868e9ab0871d1caec948b4a79bfc191b4057
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-07-15 16:37:44 -0400

    new implementation of ancestors

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -3,24 +3,32 @@ from util import Stack
 
 def earliest_ancestor(ancestors, starting_node):
     s = Stack()
-    s.push(starting_node)
+    distance = 0
+    s.push((starting_node, distance))
 
     visited = set()
-    parent = starting_node
+    parent = (starting_node, distance)
 
     neighbor = get_neighbors(starting_node, ancestors)
     if len(neighbor) < 1:
         return -1
 
     while s.size():
-        removed_parent = s.pop()
-        parent = removed_parent
+        removed_tuple = s.pop()
+
+        removed_parent = removed_tuple[0]
+
+        distance = removed_tuple[1]
+        if distance > parent[1]:
+            parent = removed_tuple
+        if distance == parent[1] and removed_parent < parent[0]:
+            parent = removed_tuple
         if removed_parent not in visited:
             visited.add(removed_parent)
-            for neighbor in get_neighbors(parent, ancestors):
-                s.push(neighbor)
+            for neighbor in get_neighbors(removed_parent, ancestors):
+                s.push((neighbor, distance + 1))
 
-    return parent
+    return parent[0]
 
 
 def get_neighbors(node, ancestors):
